491a8f1660b5a2d8d7da522ddabe88f7bcf7f26f
Jeremiah Griffin <jgrif007@ucr.edu>
2017-02-22 12:13:47 -0800

    Revert "Add custom path search to PosixExecutor"
    This reverts commit 151f7da26757c1e7217f0a3b9d809b164da8c9eb.

--- src/PosixExecutor.hpp
+++ src/PosixExecutor.hpp
@@ -21,8 +21,6 @@
 #define hpp_rshell_PosixExecutor
 
 #include "Executor.hpp"
-#include <string>
-#include <vector>
 
 namespace rshell {
 
@@ -38,24 +36,6 @@ public:
     /// \param command command to execute
     /// \return exit code of the command
     virtual int execute(ExecutableCommand& command) override;
-
-private:
-    /// \brief Gets the list of paths to search for executables
-    /// \return list of paths
-    /// \see searchPath
-    ///
-    /// Obtained from the colon-delimited PATH environment variable.
-    std::vector<std::string> searchPaths() const;
-
-    /// \brief Searches for the first matching executable in the path
-    /// \param program name of the program to search for
-    /// \return path to the program to execute on success or an empty string
-    /// on failure
-    /// \see searchPaths
-    ///
-    /// If the \p program has a slash in it, it is treated as complete and
-    /// no paths will be searched.
-    std::string searchPath(const std::string& program) const;
 };
 
 } // namespace rshell
